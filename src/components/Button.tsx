5787af1c317325d9864ffbce47c4f040d032adfb
Thomas <thomas.bouasli@gmail.com>
2023-11-30 22:44:43 -0300

    Added header to inner app

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ className, ...props }: ButtonProps) => {
-  return <button {...props} className={`bg-background rounded-md px-4 py-2 text-2xl font-bold ${className}`} />;
+  return <button {...props} className={`rounded-md bg-background px-4 py-2 text-xl font-bold ${className}`} />;
 };
 export default Button;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,12 +8,13 @@ const AppPage = () => {
     signOut(auth);
   };
   return (
-    <main className="flex h-[100dvh] w-full flex-col items-center justify-center gap-4">
-      <h1 className="text-gradient select-none text-5xl font-extrabold uppercase sm:text-7xl md:text-8xl lg:text-9xl">
-        App
-      </h1>
-      <Button onClick={handleJoin}>Leave</Button>
-    </main>
+    <div className="flex h-[100dvh] w-full flex-col items-center justify-center">
+      <header className="sticky left-0 top-0 flex h-16 w-full items-center justify-between border-b border-secondary px-4 py-2">
+        <h1 className="text-gradient select-none text-2xl font-extrabold uppercase">App</h1>
+        <Button onClick={handleJoin}>Sign Out</Button>
+      </header>
+      <main className="flex h-full w-full flex-col"></main>
+    </div>
   );
 };
 export default AppPage;
