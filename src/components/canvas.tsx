6b8a07debae8dd25e132b176e1e6db45333d9efe
Thomas <thomas.bouasli@gmail.com>
2023-12-10 17:03:32 -0300

    Revert "fix: canvas UI"
    This reverts commit d5db0c9bd5445070fa45873018bd21a724310853.

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -4,15 +4,7 @@ import { twMerge } from "tailwind-merge";
 type CanvasProps = React.CanvasHTMLAttributes<HTMLCanvasElement>;
 
 const Canvas = forwardRef<HTMLCanvasElement, CanvasProps>(({ className, ...props }, ref) => {
-  return (
-    <canvas
-      ref={ref}
-      className={twMerge("aspect-square w-full bg-background", className)}
-      height={512}
-      width={512}
-      {...props}
-    />
-  );
+  return <canvas ref={ref} className={twMerge("aspect-square w-full bg-background", className)} {...props} />;
 });
 
 Canvas.displayName = "Canvas";
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -14,10 +14,10 @@ const AppPage = () => {
         <AppHeader className="h-16" />
         <Feed className="min-h-[100dvh]" />
         <Drawer.Root className="absolute bottom-0 left-0 w-full bg-background">
-          <Drawer.Handle className="absolute bottom-full flex h-10 w-full cursor-pointer items-center justify-center bg-accent py-1">
+          <Drawer.Handle className="absolute bottom-full flex h-10 w-full cursor-pointer items-center justify-center bg-foreground/10 py-1">
             <GripHorizontal />
           </Drawer.Handle>
-          <Drawer.Content className="border-x-8 border-b-8 border-accent p-4">
+          <Drawer.Content className="border-x-8 border-b-8 border-foreground/10 p-4">
             <CanvasInput />
           </Drawer.Content>
         </Drawer.Root>
